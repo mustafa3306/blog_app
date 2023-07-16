a6cb4074d13be770154051617b007cb287f7250d
Mustafa Kök <mustafaworkkok@gmail.com>
2023-07-17 00:53:30 +0300

    authContext

--- src/context/AuthContextProvider.js
+++ src/context/AuthContextProvider.js
@@ -1,5 +1,6 @@
-import React, {useContext, CreateContext} from 'react';
-
+import React, {useContext, CreateContext, useState, useEffect} from 'react';
+import { auth, googleProvider } from "../utils/firebaseUtil";
+//creating context
 const AuthContext = createContext();
 
 //defining a function to get data from Auth context
@@ -8,17 +9,60 @@ export function useAuth(){
 }
 
 
-const AuthContextProvider = () => {
-
+const AuthContextProvider = ({children}) => {
+  const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    })
+    return unsubscribe;
+  }, []);
+  
   function signup(email, password) {
     return auth.createUserWithEmailAndPassword(email, password);
-  }  
+  }
+  
+  function login(email, password) {
+    return auth.signInWithEmailAndPassword(email,password);
+  }
 
-  return (
-    <div>
+  function logout () {
+    auth.signOut();
+  }
 
-    </div>
-  )
-}
+  function loginWithGoogle() {
+    googleProvider.setCustomParameters({ prompt: "select_account"});
+    auth.signWithPopup(googleProvider);
+  }
+
+  function resetPassword(email) {
+    return auth.sendPasswordResetEmail(email);
+  }
+
+  function updateEmail(email) {
+    return currentUser.updateEmail(email);
+  }
+
+  function updatePassword(password) {
+    return currentUser.updatePassword(password);
+  }
+
+  const values={
+    currentUser,
+    signup,
+    login,
+    logout,
+    resetPassword,
+    updatePassword,
+    updateEmail,
+    loginWithGoogle,
+  }
+
+  return <AuthContext.Provider value={values}>
+    {!loading && children}
+  </AuthContext.Provider>
+};
 
 export default AuthContextProvider
